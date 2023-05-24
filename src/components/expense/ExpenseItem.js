fab9260535c96f0f4f47b342060f701408896955
Fernando <fernandovalentedev@gmail.com>
2023-05-24 10:33:10 -0300

    wip

--- src/components/expense/ExpenseItem.js
+++ src/components/expense/ExpenseItem.js
@@ -6,7 +6,6 @@ function ExpensesItem(props) {
     return (
         <div className="expense-item">
             <ExpanseDate date={props.date} />
-
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
